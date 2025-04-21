0085a0d7ed50c4832eb6331d308c447ce0a329cd
Christian Kaindl <christiankaindl@users.noreply.github.com>
2025-04-21 21:43:37 +0200

    MatomoTrackOptions: Add descriptions for properties
    Descriptions taken from official docs: https://developer.matomo.org/api-reference/tracking-api

--- index.ts
+++ index.ts
@@ -122,110 +122,197 @@ interface MatomoSingleTrackOptions extends MatomoTrackOptions {
 }
 
 
+/**
+ * Interface for Matomo tracking options
+ */
 interface MatomoTrackOptions {
-  [key: string]: number | string | undefined
+  [key: string]: number | string | undefined;
 
-  idsite?: number
-  rec?: 1
+  /** Required: The ID of the website we're tracking a visit/action for */
+  idsite?: number;
+  /** Required for tracking, must be set to 1 */
+  rec?: 1;
 
   // Recommended parameters
+  /** The title of the action being tracked. For page tracks this is used as page title */
   action_name?: string;
+  /** The full URL for the current action */
+  url?: string;
+  /** The unique visitor ID, must be a 16 characters hexadecimal string */
   _id?: string;
+  /** Random value generated before each request to avoid caching by browser or proxy */
   rand?: string;
+  /** API version to use (currently always set to 1) */
   apiv?: 1;
 
   // Optional User info
+  /** Full HTTP Referrer URL. Used to determine how someone got to your website */
   urlref?: string;
+  /** Visit scope custom variables. JSON encoded string of the custom variable array */
   _cvar?: string;
+  /** Current count of visits for this visitor */
   _idvc?: string;
+  /** Timestamp of the previous visit */
   _viewts?: string;
+  /** Timestamp of the first visit */
   _idts?: string;
+  /** Campaign name used to attribute goal conversions */
   _rcn?: string;
+  /** Campaign keyword used to attribute goal conversions */
   _rck?: string;
+  /** The resolution of the device the visitor is using, e.g., 1280x1024 */
   res?: string;
+  /** Current hour (local time) */
   h?: number;
+  /** Current minute (local time) */
   m?: number;
+  /** Current second (local time) */
   s?: number;
+  /** Set to 1 if visitor has Flash plugin */
   fla?: 1;
+  /** Set to 1 if visitor has Java plugin */
   java?: 1;
+  /** Set to 1 if visitor has Director plugin */
   dir?: 1;
+  /** Set to 1 if visitor has Quicktime plugin */
   qt?: 1;
+  /** Set to 1 if visitor has PDF plugin */
   pdf?: 1;
+  /** Set to 1 if visitor has Windows Media plugin */
   wma?: 1;
+  /** Set to 1 if visitor has Silverlight plugin */
   ag?: 1;
+  /** Set to 1 if visitor's client supports cookies */
   cookie?: 1;
+  /** Override for User-Agent HTTP header */
   ua?: string;
+  /** Override for Accept-Language HTTP header */
   lang?: string;
+  /** User ID - any non-empty unique string identifying the user */
   uid?: string;
+  /** Visitor ID - must be exactly a 16 character hexadecimal string */
   cid?: string;
+  /** If set to 1, forces a new visit to be created for this action */
   new_visit?: number;
 
   // Optional Action info
+  /** Page scope custom variables. JSON encoded string of the custom variable array */
   cvar?: string;
+  /** An external URL the user has opened. Used for tracking outlink clicks */
   link?: string;
+  /** URL of a file the user has downloaded. Used for tracking downloads */
   download?: string;
+  /** The Site Search keyword. Tracked as a Site Search request rather than a pageview */
   search?: string;
+  /** Optional search category when search is specified */
   search_cat?: string;
+  /** Number of search results displayed on the results page */
   search_count?: number;
+  /** Six character unique ID that identifies which actions were performed on a specific page view */
   pv_id?: string;
+  /** Triggers a conversion for the goal of the website with this ID */
   idgoal?: number;
+  /** Monetary value generated as revenue by this goal conversion */
   revenue?: number;
+  /** Generation time in milliseconds */
   gt_ms?: number;
+  /** Charset of the page being tracked, if different from default utf-8 */
   cs?: string;
+  /** Custom action flag. Set to 1 for non-pageview tracking requests */
   ca?: 1;
 
   // Optional Event Tracking info
+  /** Event category. Must not be empty (e.g., Videos, Music, Games) */
   e_c?: string;
+  /** Event action. Must not be empty (e.g., Play, Pause, Download) */
   e_a?: string;
+  /** Event name (e.g., a Movie name, Song name, File name) */
   e_n?: string;
+  /** Event value. Must be a numeric value (float or integer) */
   e_v?: string;
 
   // Optional Content Tracking info
+  /** Name of the content (e.g., 'Ad Foo Bar') */
   c_n?: string;
+  /** The actual content piece (e.g., path to image, video, audio, text) */
   c_p?: string;
+  /** Target of the content (e.g., URL of a landing page) */
   c_t?: string;
+  /** Name of the interaction with the content (e.g., 'click') */
   c_i?: string;
 
   // Optional Ecommerce info
+  /** Unique string identifier for the ecommerce order */
   ec_id?: string;
+  /** JSON encoded array of items in the ecommerce order */
   ec_items?: string;
+  /** Subtotal of the order (excluding shipping) */
   ec_st?: number;
+  /** Tax amount of the order */
   ec_tx?: number;
+  /** Shipping cost of the order */
   ec_sh?: number;
+  /** Discount offered */
   ec_dt?: number;
+  /** Timestamp when ecommerce order was stored */
   _ects?: number;
 
   // Other parameters (require authentication via token_auth)
+  /** 32 character authorization key used to authenticate the API request */
   token_auth?: string;
+  /** Override value for the visitor IP (IPv4 or IPv6) */
   cip?: string;
+  /** Override for the datetime of the request (format: 2011-04-05 00:11:42 or UNIX timestamp) */
   cdt?: string;
+  /** Override value for the country (two letter lowercase country code) */
   country?: string;
+  /** Override value for the region (ISO 3166-2 region code) */
   region?: string;
+  /** Override value for the city (e.g., Tokyo) */
   city?: string;
+  /** Override value for the visitor's latitude (e.g., 22.456) */
   lat?: string;
+  /** Override value for the visitor's longitude (e.g., 22.456) */
   long?: string;
 
   // Optional Media Analytics parameters
+  /** Media ID */
   ma_id?: string;
+  /** Media title */
   ma_ti?: string;
+  /** Media resource URL */
   ma_re?: string;
+  /** Media type: "video" or "audio" */
   ma_mt?: "video" | "audio";
+  /** Media player name */
   ma_pn?: string;
+  /** Media session number */
   ma_sn?: number;
+  /** Media length/duration in seconds */
   ma_le?: number;
+  /** Media position in seconds */
   ma_ps?: number;
+  /** Time to play - how long until media started playing */
   ma_ttp?: number;
+  /** Media player width */
   ma_w?: number;
+  /** Media player height */
   ma_h?: number;
+  /** Fullscreen flag (1 for fullscreen, 0 otherwise) */
   ma_fs?: 1 | 0;
+  /** Media segment */
   ma_se?: string;
 
   // Optional Queued Tracking parameters
-  queuedtracking?: 0
+  /** Set to 0 to bypass queued tracking and execute request directly */
+  queuedtracking?: 0;
 
   // Other parameters
+  /** If set to 0, Matomo responds with HTTP 204 instead of GIF image */
   send_image?: 0;
+  /** If set to 1, tracks a heartbeat instead of new activity (updates visit duration) */
   ping?: 1;
+  /** If set to 1, enables tracking of bot requests */
   bots?: 1;
 }
 
